5fd5fcb4d0c69526866aa1837c7a4b628bff60a9
Luciano Augusto Campagnoli da Silva <lucianos@cpqd.com.br>
2021-10-23 19:21:44 -0300

    Slices emojis

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
-    server.run();
+    let string = String::from("🤣💻👾🙀");
+    let string_slice = &string[..8];
+    let string_borrow: &str = &string;
+    let string_literal = "1234";
+
+    dbg!(&string);
+    dbg!(string_slice);
+    dbg!(string_borrow);
+    dbg!(string_literal);
+
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
 }
 
 struct Server {
