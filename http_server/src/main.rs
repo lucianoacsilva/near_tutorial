ac096f2afb686e75093d59ca9f7ff6661dff739f
Luciano Augusto Campagnoli da Silva <lucianos@cpqd.com.br>
2021-10-23 19:39:43 -0300

    Plays with enums

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let get = HTTP_method::GET("abcd".to_string());
+    let post = HTTP_method::POST;
+    let put = HTTP_method::PUT;
+    let delete = HTTP_method::DELETE(100);
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -17,6 +22,24 @@ impl Server {
     fn run(self) {
         println!("Server listening on {}!", self.address);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: HTTP_method
+}
+
+enum HTTP_method {
+    GET(String),
+    POST,
+    PUT,
+    DELETE(u64),
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }
 
 
