6e35c608ac8b0b2ba6b867951cc90807b21e7764
lucianoacsilva <luti4497@gmail.com>
2021-10-30 16:19:49 -0300

    Implemetns customized error

--- http_server/src/http/mod.rs
+++ http_server/src/http/mod.rs
@@ -1,5 +1,5 @@
 pub use request::Request;
 pub use method::HTTP_method;
-
+pub use request::ParseError;
 pub mod request;
 pub mod method;
--- http_server/src/http/request.rs
+++ http_server/src/http/request.rs
@@ -1,10 +1,12 @@
 use super::method::HTTP_method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
-    method: HTTP_method
+    method: HTTP_method,
 }
 
 impl Request {
@@ -14,10 +16,43 @@ impl Request {
 }
 
 impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
 
-    type Error = String;
+    // GET /search?name=abc&sort=1 HTTP/1.1
 
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!();
     }
 }
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError{
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request!",
+            Self::InvalidEncoding => "Invalid Encoding!",
+            Self::InvalidProtocol => "Invalid Protocol!",
+            Self::InvalidMethod => "Invalid Method!"
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
