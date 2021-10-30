1e880fec5e8adbe7deb2f116c62d4b4ac7227703
lucianoacsilva <luti4497@gmail.com>
2021-10-30 11:46:12 -0300

    Creates request.ts

--- http_server/src/http/request.rs
+++ http_server/src/http/request.rs
@@ -1,7 +1,23 @@
 use super::method::HTTP_method;
+use std::convert::TryFrom;
 
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: HTTP_method
-        }
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: HTTP_method
+}
+
+impl Request {
+    fn from_byte_array(buffer: &[u8]) -> Result<Self, String> {
+        unimplemented!();
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+
+    type Error = String;
+
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!();
+    }
+}
