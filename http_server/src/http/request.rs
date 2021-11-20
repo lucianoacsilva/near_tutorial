e69a139c19e68ed4071d84645390cf715e81491b
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2021-11-19 23:16:46 -0300

    Advanced error handling

--- http_server/src/http/request.rs
+++ http_server/src/http/request.rs
@@ -1,7 +1,9 @@
+use std::str::Utf8Error;
 use super::method::HTTP_method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
 
 pub struct Request {
     path: String,
@@ -21,6 +23,8 @@ impl TryFrom<&[u8]> for Request {
     // GET /search?name=abc&sort=1 HTTP/1.1
 
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buffer)?;
+
         unimplemented!();
     }
 }
@@ -46,6 +50,12 @@ impl ParseError{
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     address: String,
@@ -22,6 +25,12 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(Request) => {},
+                                Err(e) => println!("Failed to parse received request: {}", e)
+                            }
+
                         },
                         Err(e) => println!("Failed to read from connecdtion: {}", e)
                     }
